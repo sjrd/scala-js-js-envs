scala-js/scala-js

Refactor JSEnv to support async running

tools/shared/src/main/scala/scala/scalajs/tools/env/AsyncJSRunner.scala
@@ -1,0 +1,12 @@
+package scala.scalajs.tools.env
+
+trait AsyncJSRunner {
+  /** Start the associated run and return. */
+  def run(): Unit
+
+  /** Checks whether this async runner is still running */
+  def isRunning(): Boolean
+
+  /** Await completion of the started Run. Throws if the run failed */
+  def await(): Unit
+}

sjrd/scala-js-js-envs

tools/shared/src/main/scala/scala/scalajs/tools/env/AsyncJSRunner.scala
@@ -1,0 +1,12 @@
+package scala.scalajs.tools.env
+
+trait AsyncJSRunner {
+  /** Start the associated run and return. */
+  def start(): Unit
+
+  /** Checks whether this async runner is still running */
+  def isRunning(): Boolean
+
+  /** Await completion of the started Run. Throws if the run failed */
+  def await(): Unit
+}