5ef41be9bc38622e248e75f134aa0189cc69fd66
Sébastien Doeraene <sjrdoeraene@gmail.com>
2017-03-27 23:33:01 +0200

    Separate all PhantomJS-related things in distinct projects.
    This is part of #2839.
    
    * `PhantomJSEnv` and `RetryingComJSEnv` are sent to their own
      `phantomJSEnv` sbt project.
    * The settings and tasks used to construct the `ClassLoader` that
      loads jetty8 are extracted into a separate `AutoPlugin`, named
      `PhantomJSEnvPlugin`, in the sbt project `phantomJSEnvPlugin`.
    * The tests in `sbt-plugin-test` do not mix PhantomJS-related tests
      with other tests anymore (in particular `jsDependencies`).

--- js-envs-test-suite/src/test/scala/org/scalajs/jsenv/test/PhantomJSWithCustomInitFilesTest.scala
+++
@@ -1,9 +0,0 @@
-package org.scalajs.jsenv.test
-
-import org.scalajs.jsenv.phantomjs.PhantomJSEnv
-
-class PhantomJSWithCustomInitFilesTest extends CustomInitFilesTest {
-  protected def newJSEnv: PhantomJSEnv = new PhantomJSEnv {
-    override def customInitFiles() = makeCustomInitFiles()
-  }
-}
